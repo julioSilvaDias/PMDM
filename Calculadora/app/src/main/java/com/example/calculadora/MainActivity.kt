e7e4c45592340d61fb06d29012ef8c9019f68754
julioSilvaDias <juliocesar.silvadi@elorrieta-errekamari>
2024-09-14 22:42:21 +0200

    Calculadora en koltim
    -la vista esta terminada, tanto la vista vertical como la horizontal
    -de la logica ya he declarado variables referenciando a los botones de la vista
    -el boton 0 ya tiene un evento y ya escribe en el EditText

---
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val editText = findViewById<EditText>(R.id.editTextNumber)
+        val num0 = findViewById<Button>(R.id.buttonNum0)
+        val num1 = findViewById<Button>(R.id.buttonNum1)
+        val num2 = findViewById<Button>(R.id.buttonNum2)
+        val num3 = findViewById<Button>(R.id.buttonNum3)
+        val num4 = findViewById<Button>(R.id.buttonNum4)
+        val num5 = findViewById<Button>(R.id.buttonNum5)
+        val num6 = findViewById<Button>(R.id.buttonNum6)
+        val num7 = findViewById<Button>(R.id.buttonNum7)
+        val num8 = findViewById<Button>(R.id.buttonNum8)
+        val num9 = findViewById<Button>(R.id.buttonNum9)
+        val btnComa = findViewById<Button>(R.id.buttonComa)
+        val btnIgual = findViewById<Button>(R.id.buttonIgual)
+        val btnSum = findViewById<Button>(R.id.buttonMas)
+        val btnRes = findViewById<Button>(R.id.buttonMenos)
+        val btnMult = findViewById<Button>(R.id.buttonMultiplicacion)
+        val btnDiv = findViewById<Button>(R.id.buttonDivision)
+        val btnCompart = findViewById<Button>(R.id.buttonCompartir)
+        val btnAc = findViewById<Button>(R.id.buttonAc)
+
+        num0.setOnClickListener{
+            var textoActual = editText.text.toString()
+            editText.setText(textoActual + "0")
+        }
+    }
+}
